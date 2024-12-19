a9d1eb1510d4064fe0c225b849fadcefb26cc71a
Arati Prasad <aratiprasad778@gmail.com>
2024-12-19 17:14:10 +0530

    ading domain

--- Practise/server.js
+++ Practise/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const contactsRoutes = require("./contactsRoutes");
 const userRoutes = require("./userRoutes");
 const errorHandler = require("./midleware/errorhandler");
 const connectDb = require("./config/dbConnection");
@@ -8,24 +9,40 @@ const projectRoutes = require("./routes/projectRoutes");
 const commentRouter = require("./routes/commentRoutes");
 const likeRouter = require("./routes/likeRoutes");
 const forumPost = require("./routes/postRoutes");
+const notification = require("./routes/notificationRoutes");
 const refreshToken = require("./routes/refreshTokenRoutes");
-const validatePermission = require("./routes/validatePermissionRoutes");
 
+// Connect to the database
 connectDb();
 
 const app = express();
-app.use(cors());
 
-const port = process.env.PORT || 4000;
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Enable CORS for all routes
+// app.use(cors());
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: ["https://comicplane.site"], // Allowed origins
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
+    credentials: true, // If you're using cookies or sessions
+  })
+);
+
+const port = process.env.PORT || 5000;
 
 // Route middlewares
+app.use("/api/contacts", contactsRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/comments", commentRouter);
 app.use("/api/like", likeRouter);
 app.use("/api/posts", forumPost);
+app.use("/api/notification", notification);
 app.use("/api/refresh-token", refreshToken);
-app.use("/api/resource", validatePermission);
 app.use(errorHandler);
 
 // Start the Express server
